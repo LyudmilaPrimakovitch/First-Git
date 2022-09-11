0abaff3fd8a97d4214d2c4fc8a34001c7a1de394
Luda <lyudmilaprimakovitch@yandex.by>
2022-09-11 15:16:24 +0300

    C1

---
+++ ProgSWAPI.py
@@ -0,0 +1,10 @@
+import requests
+import re
+
+
+for i in range (1,15): 
+    web_url = requests.get('https://swapi.dev/api/people/' + str(i))
+    f=open('my_file.txt','a')
+    f.write(web_url.json()["name"])
+    f.write("\n")
+    f.close()
